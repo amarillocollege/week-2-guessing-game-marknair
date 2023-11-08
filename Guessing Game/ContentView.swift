2b2bc711505a0a650b6bf40b98c2a364b1f18f2b
Mark Nair <marknair@me.com>
2023-11-08 11:59:57 -0600

    Final changes

--- Guessing Game/ContentView.swift
+++ Guessing Game/ContentView.swift
@@ -9,16 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var birds = ["Blue Pita", "Chatham Albatross", "Dunlin", "Eurasian Griffon", "Great Gray Owl", "Inca Tern", "Piping Plover", "Zitting Cristicola"]
+    @State private var birds = ["Blue Pita", "Chatham Albatross", "Dunlin", "Eurasian Griffon", "Great Gray Owl", "Inca Tern", "Piping Plover", "Zitting Cristicola"].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
     
     var body: some View {
-        VStack {
-            Image(systemName: "questionmark.app.fill")
-                .imageScale(.large)
-                .foregroundColor(.mint)
-            Text("The Guessing Game Template")
+        
+        ZStack {
+            Color.mint
+                .ignoresSafeArea()
+            VStack {
+                VStack {
+                    Text("Which bird is the")
+                        .foregroundStyle(.white)
+                    Text(birds[correctAnswer])
+                        .foregroundStyle(.white)
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(birds[number])
+                        }
+                    }
+                }
+            }
         }
-        .padding()
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestions)
+        } message: {
+            Text("Your score is ???")
+        }
+    }
+    
+    //MARK: Methods
+    
+    func askQuestions() {
+        birds.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
     }
 }
 
